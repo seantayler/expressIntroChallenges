5621b0a3a06a7c7d9f0d629494f71cddf91d80e2
Sean Tayler <seanjtayler@gmail.com>
2018-09-13 16:07:50 -0700

    completed

--- part1/server.js
+++ part1/server.js
@@ -6,6 +6,34 @@ app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
 
+// Challenge 1:
+// Create a GET route for "/hello" that returns the response "Hello!".
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
+});
+
+// Challenge 2:
+// Create a POST route for "/create/:name" that returns a json object that looks like this: { "id":1, "name":"the name they pass in the route" }
+app.post('/create/:name', function(req, res) {
+  let obj = { "id":1, "name":`${req.params.name}` }
+  res.json(obj);
+});
+
+// Challenge 3:
+// Create a GET route for "/" that returns the index.html file.
+app.get('/', function(req, res) {
+  res.sendFile(__dirname+'/index.html')
+});
+
+// Challenge 4:
+// Create a GET route for "/verify/:age" that checks if the age is greater than 13. If it is then it sends a 200 status response (for all good). If it is not greater than 13 then send a 403 status code. For more information on what status codes mean refer to this - https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+app.get('/verify/:age', (req, res)=> {
+  if (req.params.age > 13){
+    res.sendStatus(200)
+  }
+  res.sendStatus(403)
+})
+
 app.use(function(req, res) {
   res.sendStatus(404);
 });
--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,46 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+let fs = require('fs');
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
+
+// Challenge 1:
+// Create a POST route for "/create/:name/:age" that creates an object that looks like this: { "name": "troy", "age": 20 } Then take that object and insert it into storage.json
+app.post('/create/:name/:age', (req,res)=>{
+  let storage = fs.readFileSync(__dirname+'/storage.json', 'utf8')
+  let data = JSON.parse(storage)
+  let obj = {name: req.params.name, age:req.params.age};
+
+  data.push(obj);
+  fs.writeFileSync(__dirname+'/storage.json', JSON.stringify(data));
+  res.send(data)
+})
+
+// Challenge 2:
+// Create a Get route for "/" that returns all of the objects inside storage.json.
+app.get('/', (req,res)=>{
+  let objects = fs.readFileSync('storage.json', 'utf8');
+  res.send(objects);
+})
+
+// Challenge 3:
+// Create a Get route for "/:name" that returns the first object in storage.json that matches the name. If there is no object in storage.json that matches then return a 400 status.
+app.get('/:name', (req,res)=>{
+  let arr = JSON.parse(fs.readFileSync(__dirname+'/storage.json', 'utf8'))
+  let user = arr.find(el => el.name === req.params.name)
+
+  if (user){
+    res.send(user)
+  }
+  res.sendStatus(400)
+})
+
+
+// Challenge 4 (stretch):
+// Modify your logic so every object has and id field that automatically goes up by one for every object inserted (first object has an id of 1, second object has an id of 2 ect). Then modify challenge 3 so that it finds the object by an id instead of by name.
 
 app.use(function(req, res) {
   res.sendStatus(404);
